6be5b32d9536726963c178ad99d10f4b914ac1bb
Kulkarni-ui <158289522+Kulkarni-ui@users.noreply.github.com>
2025-02-28 23:32:34 +0530

    App.js

---
+++ App.js
@@ -0,0 +1,12 @@
+import React from "react";
+import ProductGallery from "./components/ProductGallery";
+
+function App() {
+  return (
+    <div>
+      <ProductGallery />
+    </div>
+  );
+}
+
+export default App;
